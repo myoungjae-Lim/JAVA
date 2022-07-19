8e4470f3482cbd621dfe81a1a18bb9f9c2830a33
myoungjae <58833220+myoungjae-Lim@users.noreply.github.com>
2022-07-19 22:05:26 +0900

    Add files via upload

---
+++ LMJ/MJ/MusicProject/src/PlayMP3.java
@@ -0,0 +1,60 @@
+import javazoom.jl.player.Player;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+
+public class PlayMP3 {
+    public static void main(String[] args) {
+
+        String filename = "G:\\lmj\\2 이누야샤-달묘전설.mp3";
+        MP3Player mp3Player = new MP3Player(filename);
+        mp3Player.play();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Write stop to stop the music: ");
+
+        if (sc.nextLine().equalsIgnoreCase("stop")) {
+            mp3Player.close();
+        }
+
+    }
+}
+
+class MP3Player {
+    private final String mp3FileToPlay;
+    private Player jlPlayer;
+
+    public MP3Player(String mp3FileToPlay) {
+        this.mp3FileToPlay = mp3FileToPlay;
+    }
+
+    public void play() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(mp3FileToPlay);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            jlPlayer = new Player(bufferedInputStream);
+        } catch (Exception e) {
+            System.out.println("Problem playing mp3 file " + mp3FileToPlay);
+            System.out.println(e.getMessage());
+        }
+
+        new Thread() {
+            public void run() {
+                try {
+                    jlPlayer.play();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }.start();
+
+
+    }
+    
+    public void close() {
+        if (jlPlayer != null) jlPlayer.close();
+    }
+}
